54d2e316015a92ca883591e6ff1794ba4568d850
Camilo_Zuluaga <camilo.zuluaga.trader@gmail.com>
2019-08-07 08:19:58 -0500

    Adding class and inputs

--- Tick_Data.py
+++ Tick_Data.py
@@ -1,67 +1,78 @@
 import pandas as pd
 from datetime import datetime, timedelta 
 import sys
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from ib_insync import *
 
-ib = IB()
-ib.connect('127.0.0.1', 7497, clientId=17)
-contract = Future("ES", "201909", "GLOBEX")
+class Topo:
 
-days= {0: 'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 6:'Sunday'}
+    def __init__(self):
+        self.clientid = input('\tID: ')
+        self.ticket = input('\tTicket: ')
+        self.last = input('\tContract Expiration: ')
+        self.exch = input('\tExchange: ')
+        self.contract = Future(self.ticket, self.last, self.exch)
+        self.days= {0: 'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 6:'Sunday'}
 
-def empty_df():
-    global df
-    df= pd.DataFrame(columns=['time','bid', 'bidSize', 'ask', 'askSize'])
+    def connect(self):
+        ib.connect('127.0.0.1', 7497, clientId=self.clientid)
 
-def onPendingTickers(ticker):
-    """When the request of real time data is updated this function stores the date of the returned data,
-    and creates a new Data Frame for 5 secs time bars"""
+    def empty_df(self):
+        self.df= pd.DataFrame(columns=['time','bid', 'bidSize', 'ask', 'askSize'])
 
-    global df, df2
-    df2 = util.df(ticker)
-    df = df.append(df2.loc[:,['time','bid', 'bidSize', 'ask', 'askSize']])
+    def onPendingTickers(self, ticker):
+        """When the request of real time data is updated this function stores the date of the returned data,
+        and creates a new Data Frame for 5 secs time bars"""
 
-def recorder():
-    global ticker
-    ticker = ib.reqTickByTickData(contract, 'BidAsk')
-    ib.pendingTickersEvent += onPendingTickers
+    #global df, df2
+        self.df2 = util.df(ticker)
+        self.df = self.df.append(self.df2.loc[:,['time','bid', 'bidSize', 'ask', 'askSize']])
 
-def cancelation():
-    ib.cancelTickByTickData(ticker.contract, 'BidAsk')
+    def recorder(self):
+        #global ticker
+        self.ticker = ib.reqTickByTickData(self.contract, 'BidAsk')
+        ib.pendingTickersEvent += self.onPendingTickers
 
-def savedata(now):
-    df.to_csv('C:/Users/MiloZB/Desktop/ES_{}_{}_{}.csv'.format(days[now.weekday()],now.hour,now.minute))
+    def cancelation(self):
+        ib.cancelTickByTickData(self.ticker.contract, 'BidAsk')
 
-def current_time():
-    global now
-    now = datetime.now()
-    #Market Close
-    if ((now.weekday() == 0)|(now.weekday() == 1)|(now.weekday() == 2) |(now.weekday() == 3)) & (now.hour == 15) & (now.minute == 14) & (now.second == 0):
-        sun_open.pause()
-        cancelation()
-        savedata(now)
-    #Market Open
-    elif ((now.weekday() == 0)|(now.weekday() == 1)|(now.weekday() == 2) |(now.weekday() == 3)) & (now.hour == 15) & (now.minute == 31) & (now.second == 0):
-        empty_df()
-        recorder()
-    #Market Friday Close
-    elif (now.weekday()==4) & (now.hour == 15) & (now.minute == 59):
-        cancelation()
-        savedata(now)
-        time_job.pause()
-        try:
-            sys.exit()
-        except:
-            print('Market is closed: Please press Ctrl + C to exit')
+    def savedata(self):
+        self.df.to_csv('C:/Users/MiloZB/Desktop/ES_{}_{}_{}.csv'.format(self.days[self.now.weekday()],self.now.hour,self.now.minute))
+
+    def current_time(self):
+        self.now = datetime.now()
+        #Market Intraday Close
+        if ((self.now.weekday() == 0)|(self.now.weekday() == 1)|(self.now.weekday() == 2) |(self.now.weekday() == 3)) & (self.now.hour == 8) & (self.now.minute == 16) & (self.now.second == 20):
+            sun_open.pause()
+            self.cancelation()
+            self.savedata()
+        #Market Intraday Open
+        elif ((self.now.weekday() == 0)|(self.now.weekday() == 1)|(self.now.weekday() == 2) |(self.now.weekday() == 3)) & (self.now.hour == 8) & (self.now.minute == 16) & (self.now.second == 50):
+            self.empty_df()
+            self.recorder()
+        #Market Friday Close
+        elif (self.now.weekday()==2) & (self.now.hour == 8) & (self.now.minute == 17):
+            self.cancelation()
+            self.savedata()
+            ib.disconnect()
+            time_job.pause()
+            try:
+                sys.exit()
+            except:
+                print('Market is closed: Please press Ctrl + C to exit')
 
 if __name__ == '__main__':
 
-    empty_df()
-    current_time()
+    ib = IB()
+    juan = Topo()
+
+    juan.connect()
+    juan.empty_df()
+
     scheduler = BackgroundScheduler()
-    time_job = scheduler.add_job(current_time, 'interval', seconds=1)
-    sun_open = scheduler.add_job(recorder, 'cron', day_of_week=1, hour=14, minute=30) #Market Sunday Open
+    time_job = scheduler.add_job(juan.current_time, 'interval', seconds=1)
+    sun_open = scheduler.add_job(juan.recorder, 'cron', day_of_week=2, hour=8, minute=16) #Market Sunday Open
     scheduler.start()
 
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
